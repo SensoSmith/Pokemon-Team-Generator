42a48c80d56ae9bdfbfc1ed6507b418aa24f889e
SensoSmith <sensosmith@gmail.com>
2022-06-03 15:38:12 -0500

    Added Type Coverage
    - Added the coverage attribute to the pokemon class which stores a list of all the move types the pokemon has a valid move for
    - Added a function to verify that all type combinations in the Pokedex are covered by the types of moves accessible by the team
    - Added a function to select the game version and remove inaccessible pokemon accordingly
    - Added the final 19 pokemon, bringing to the total amount to 79

--- Main.py
+++ Main.py
@@ -31,72 +31,92 @@ strength = HM("Strength")
 flash = HM("Flash")
 
 class pokemon:
-    def __init__(self, name, number, types, HMs):
+    def __init__(self, name, number, types, HMs, coverage):
         self.name = name
         self.number = number
         self.types = types
         self.HMs = HMs
+        self.coverage = coverage
 
-Venusaur = pokemon("Venusaur", 3, [Grass, Poison], [cut])
-Charizard = pokemon("Charizard", 6, [Fire, Flying], [cut, strength])
-Blastoise = pokemon("Blastoise", 9, [Water], [surf, strength])
-Butterfree = pokemon("Butterfree", 12, [Bug, Flying], [])
-Beedrill = pokemon("Beedrill", 15, [Bug, Poison], [cut])
-Pidgeot = pokemon("Pidgeot", 18, [Normal, Flying], [fly])
-Raticate = pokemon("Raticate", 20, [Normal], [])
-Fearow = pokemon("Fearow", 22, [Normal, Flying], [fly])
-Arbok = pokemon("Arbok", 24, [Poison], [strength])
-Raichu = pokemon("Raichu", 26, [Electric], [flash])
-Sandslash = pokemon("Sandslash", 28, [Ground], [cut, strength])
-Nidoqueen = pokemon("Nidoqueen", 31, [Poison, Ground], [surf, strength])
-Nidoking = pokemon("Nidoking", 34, [Poison, Ground], [surf, strength])
-Clefable = pokemon("Clefable", 36, [Normal], [strength, flash])
-Ninetales = pokemon("Ninetales", 38, [Fire], [])
-Wigglytuff = pokemon("Wigglytuff", 40, [Normal], [strength, flash])
-Golbat = pokemon("Golbat", 42, [Poison, Flying], [])
-Vileplume = pokemon("Vileplume", 45, [Poison, Grass], [cut])
-Parasect = pokemon("Parasect", 47, [Bug, Grass], [cut])
-Venomoth = pokemon("Venomoth", 49, [Bug, Poison], [])
-Dugtrio = pokemon("Dugtrio", 51, [Ground], [])
-Persian = pokemon("Persian", 53, [Normal], [])
-Golduck = pokemon("Golduck", 55, [Water], [surf, strength])
-Primeape = pokemon("Primeape", 57, [Fighting], [strength])
-Arcanine = pokemon("Arcanine", 59, [Fire], [])
-Poliwrath = pokemon("Poliwrath", 62, [Water, Fighting], [surf, strength])
-Alakazam = pokemon("Alakazam", 65, [Psychic], [flash])
-Machamp = pokemon("Machamp", 68, [Fighting], [strength])
-Victreebel = pokemon("Victreebel", 71, [Grass, Poison], [cut])
-Tentacruel = pokemon("Tentacruel", 73, [Water, Poison], [cut, surf])
-Golem = pokemon("Golem", 76, [Rock, Ground], [strength])
-Rapidash = pokemon("Rapidash", 78, [Fire], [])
-Slowbro = pokemon("Slowbro", 80, [Water, Psychic], [surf, strength, flash])
-Magneton = pokemon("Magneton", 82, [Electric], [flash])
-Farfetchd = pokemon("Farfetch'd", 83, [Normal, Flying], [cut, fly])
-Dodrio = pokemon("Dodrio", 85, [Normal, Flying], [fly])
-Dewgong = pokemon("Dewgong", 87, [Water, Ice], [surf, strength])
-Muk = pokemon("Muk", 89, [Poison], [])
-Cloyster = pokemon("Cloyster", 91, [Water, Ice], [surf])
-Gengar = pokemon("Gengar", 94, [Ghost, Poison], [strength])
-Onix = pokemon("Onix", 95, [Rock, Ground], [strength])
-Hypno = pokemon("Hypno", 97, [Psychic], [flash])
-Kingler = pokemon("Kingler", 99, [Water], [cut, surf, strength])
-Electrode = pokemon("Electrode", 101, [Electric], [flash])
-Exeggutor = pokemon("Exeggutor", 103, [Grass, Psychic], [strength])
-Marowak = pokemon("Marowak", 105, [Ground], [strength])
-Hitmonlee = pokemon("Hitmonlee", 106, [Fighting], [strength])
-Hitmonchan = pokemon("Hitmonchan", 107, [Fighting], [strength])
-Lickitung = pokemon("Lickitung", 108, [Normal], [cut, surf, strength])
-Weezing = pokemon("Weezing", 110, [Poison], [])
-MrMime = pokemon("Mr. Mime", 122, [Psychic], [flash])
-Jynx = pokemon("Jynx", 124, [Ice, Psychic], [])
-Pinsir = pokemon("Pinsir", 127, [Bug], [cut, strength])
-Vaporeon = pokemon("Vaporeon", 134, [Water], [surf])
-Jolteon = pokemon("Jolteon", 135, [Electric], [flash])
-Flareon = pokemon("Flareon", 136, [Fire], [])
-Omastar = pokemon("Omastar", 139, [Rock, Water], [surf])
-Kabutops = pokemon("Kabutops", 141, [Rock, Water], [surf])
-Snorlax = pokemon("Snorlax", 143, [Normal], [surf, strength])
-Dragonite = pokemon("Dragonite", 149, [Dragon, Flying], [surf, strength])
+Venusaur = pokemon("Venusaur", 3, [Grass, Poison], [cut], [Grass])
+Charizard = pokemon("Charizard", 6, [Fire, Flying], [cut, strength], [Fire, Fighting, Ground])
+Blastoise = pokemon("Blastoise", 9, [Water], [surf, strength], [Water, Fighting, Ground, Ice])
+Butterfree = pokemon("Butterfree", 12, [Bug, Flying], [], [Grass, Psychic])
+Beedrill = pokemon("Beedrill", 15, [Bug, Poison], [cut], [Bug])
+Pidgeot = pokemon("Pidgeot", 18, [Normal, Flying], [fly], [Flying])
+Raticate = pokemon("Raticate", 20, [Normal], [], [Water, Electric, Ground, Ice])
+Fearow = pokemon("Fearow", 22, [Normal, Flying], [fly], [Flying])
+Arbok = pokemon("Arbok", 24, [Poison], [strength], [Rock, Ground])
+Raichu = pokemon("Raichu", 26, [Electric], [flash], [Electric, Fighting])
+Sandslash = pokemon("Sandslash", 28, [Ground], [cut, strength], [Fighting, Rock, Ground])
+Nidoqueen = pokemon("Nidoqueen", 31, [Poison, Ground], [surf, strength], [Fire, Water, Electric, Fighting, Rock, Ground, Ice])
+Nidoking = pokemon("Nidoking", 34, [Poison, Ground], [surf, strength], [Fire, Water, Electric, Fighting, Rock, Ground, Ice])
+Clefable = pokemon("Clefable", 36, [Normal], [strength, flash], [Fire, Water, Electric, Grass, Fighting, Psychic, Ice])
+Ninetales = pokemon("Ninetales", 38, [Fire], [], [Fire, Ground])
+Wigglytuff = pokemon("Wigglytuff", 40, [Normal], [strength, flash], [Fire, Water, Electric, Grass, Fighting, Psychic, Ice])
+Golbat = pokemon("Golbat", 42, [Poison, Flying], [], [])
+Vileplume = pokemon("Vileplume", 45, [Poison, Grass], [cut], [Grass])
+Parasect = pokemon("Parasect", 47, [Bug, Grass], [cut], [Grass, Ground])
+Venomoth = pokemon("Venomoth", 49, [Bug, Poison], [], [Grass, Psychic])
+Dugtrio = pokemon("Dugtrio", 51, [Ground], [], [Rock, Ground])
+Persian = pokemon("Persian", 53, [Normal], [], [Water, Electric])
+Golduck = pokemon("Golduck", 55, [Water], [surf, strength], [Water, Fighting, Ground, Psychic, Ice])
+Primeape = pokemon("Primeape", 57, [Fighting], [strength], [Electric, Fighting, Rock, Ground])
+Arcanine = pokemon("Arcanine", 59, [Fire], [], [Fire, Ground])
+Poliwrath = pokemon("Poliwrath", 62, [Water, Fighting], [surf, strength], [Water, Fighting, Ground, Ice])
+Alakazam = pokemon("Alakazam", 65, [Psychic], [flash], [Fighting, Ground, Psychic])
+Machamp = pokemon("Machamp", 68, [Fighting], [strength], [Fire, Fighting, Rock, Ground])
+Victreebel = pokemon("Victreebel", 71, [Grass, Poison], [cut], [Grass])
+Tentacruel = pokemon("Tentacruel", 73, [Water, Poison], [cut, surf], [Water, Ice])
+Golem = pokemon("Golem", 76, [Rock, Ground], [strength], [Fire, Fighting, Rock, Ground])
+Rapidash = pokemon("Rapidash", 78, [Fire], [], [Fire])
+Slowbro = pokemon("Slowbro", 80, [Water, Psychic], [surf, strength, flash], [Fire, Water, Fighting, Ground, Psychic, Ice])
+Magneton = pokemon("Magneton", 82, [Electric], [flash], [Electric])
+Farfetchd = pokemon("Farfetch'd", 83, [Normal, Flying], [cut, fly], [Flying])
+Dodrio = pokemon("Dodrio", 85, [Normal, Flying], [fly], [Flying])
+Dewgong = pokemon("Dewgong", 87, [Water, Ice], [surf, strength], [Water, Ice])
+Muk = pokemon("Muk", 89, [Poison], [], [Fire, Electric, Poison])
+Cloyster = pokemon("Cloyster", 91, [Water, Ice], [surf], [Water, Ice])
+Gengar = pokemon("Gengar", 94, [Ghost, Poison], [strength], [Electric, Fighting, Psychic])
+Onix = pokemon("Onix", 95, [Rock, Ground], [strength], [Rock, Ground])
+Hypno = pokemon("Hypno", 97, [Psychic], [flash], [Fighting, Psychic])
+Kingler = pokemon("Kingler", 99, [Water], [cut, surf, strength], [Water, Ice])
+Electrode = pokemon("Electrode", 101, [Electric], [flash], [Electrode])
+Exeggutor = pokemon("Exeggutor", 103, [Grass, Psychic], [strength], [Grass, Psychic])
+Marowak = pokemon("Marowak", 105, [Ground], [strength], [Fire, Water, Fighting, Ground, Ice])
+Hitmonlee = pokemon("Hitmonlee", 106, [Fighting], [strength], [Fighting])
+Hitmonchan = pokemon("Hitmonchan", 107, [Fighting], [strength], [Fire, Electric, Fighting, Ice])
+Lickitung = pokemon("Lickitung", 108, [Normal], [cut, surf, strength], [Fire, Water, Electric, Fighting, Ground, Ice])
+Weezing = pokemon("Weezing", 110, [Poison], [], [Fire, Electric, Poison])
+Rhydon = pokemon("Rhydon", 112, [Ground, Rock], [surf, strength], [Fire, Water, Electric, Fighting, Rock, Ground, Ice])
+Chansey = pokemon("Chansey", 113, [Normal], [strength, flash], [Fire, Water, Electric, Grass, Fighting, Psychic, Ice])
+Tangela = pokemon("Tangela", 114, [Grass], [cut], [Grass])
+Kangaskhan = pokemon("Kangaskhan", 115, [Normal], [surf, strength], [Fire, Water, Electric, Fighting, Rock, Ground, Ice])
+Seadra = pokemon("Seadra", 117, [Water], [surf], [Water, Ice])
+Seaking = pokemon("Seaking", 119, [Water], [surf], [Water, Ice])
+Starmie = pokemon("Starmie", 121, [Water, Psychic], [surf, flash], [Water, Electric, Psychic, Ice])
+MrMime = pokemon("Mr. Mime", 122, [Psychic], [flash], [Electric, Grass, Fighting, Psychic])
+Scyther = pokemon("Scyther", 123, [Bug, Flying], [cut], [])
+Jynx = pokemon("Jynx", 124, [Ice, Psychic], [], [Water, Fighting, Psychic, Ice])
+Electabuzz = pokemon("Electabuzz", 125, [Electric], [strength, flash], [Electric, Fighting, Psychic])
+Magmar = pokemon("Magmar", 126, [Fire], [strength], [Fire, Fighting, Psychic])
+Pinsir = pokemon("Pinsir", 127, [Bug], [cut, strength], [Fighting])
+Tauros = pokemon("Tauros", 128, [Normal], [strength], [Fire, Electric, Ground, Ice])
+Gyarados = pokemon("Gyarados", 130, [Water, Flying], [surf, strength], [Fire, Water, Electric, Ice])
+Lapras = pokemon("Lapras", 131, [Water, Ice], [surf, strength], [Water, Electric, Grass, Psychic, Ice])
+Ditto = pokemon("Ditto", 132, [Normal], [], [])
+Vaporeon = pokemon("Vaporeon", 134, [Water], [surf], [Water, Ice])
+Jolteon = pokemon("Jolteon", 135, [Electric], [flash], [Electric, Fighting])
+Flareon = pokemon("Flareon", 136, [Fire], [], [Fire])
+Porygon = pokemon("Porygon", 137, [Normal], [flash], [Electric, Psychic, Ice])
+Omastar = pokemon("Omastar", 139, [Rock, Water], [surf], [Water, Fighting, Ice])
+Kabutops = pokemon("Kabutops", 141, [Rock, Water], [surf], [Water, Fighting, Ice])
+Aerodactyl = pokemon("Aerodactyl", 142, [Rock, Flying], [fly], [Fire, Flying])
+Snorlax = pokemon("Snorlax", 143, [Normal], [surf, strength], [Fire, Water, Electric, Grass, Fighting, Rock, Ground, Psychic, Ice])
+Articuno = pokemon("Articuno", 144, [Ice, Flying], [fly], [Water, Flying, Ice])
+Zapdos = pokemon("Zapdos", 145, [Electric, Flying], [fly, flash], [Electric, Flying])
+Moltres = pokemon("Moltres", 146, [Fire, Flying], [fly], [Fire, Flying])
+Dragonite = pokemon("Dragonite", 149, [Dragon, Flying], [surf, strength], [Fire, Water, Electric, Ice])
 
 totalPokemonList = [Venusaur, Charizard, Blastoise, Butterfree, Beedrill, Pidgeot, Raticate, Fearow,
                     Arbok, Raichu, Sandslash, Nidoqueen, Nidoking, Clefable, Ninetales, Wigglytuff,
@@ -104,12 +124,14 @@ totalPokemonList = [Venusaur, Charizard, Blastoise, Butterfree, Beedrill, Pidgeo
                     Arcanine, Poliwrath, Alakazam, Machamp, Victreebel, Tentacruel, Golem, Rapidash,
                     Slowbro, Magneton, Farfetchd, Dodrio, Dewgong, Muk, Cloyster, Gengar,
                     Onix, Hypno, Kingler, Electrode, Exeggutor, Marowak, Hitmonlee, Hitmonchan,
-                    Lickitung, Weezing, MrMime, Jynx, Pinsir, Vaporeon, Jolteon, Flareon,
-                    Omastar, Kabutops, Snorlax, Dragonite]
+                    Lickitung, Weezing, Rhydon, Chansey, Tangela, Kangaskhan, Seadra, Seaking,
+                    Starmie, MrMime, Scyther, Jynx, Electabuzz, Magmar, Pinsir, Tauros,
+                    Gyarados, Lapras, Ditto, Vaporeon, Jolteon, Flareon, Porygon, Omastar,
+                    Kabutops, Aerodactyl, Snorlax, Articuno, Zapdos, Moltres, Dragonite]
 
 availablePokemonList = list(totalPokemonList)
 
-starters = [Venusaur, Charizard, Blastoise]
+gameVersion = "Error"
 
 party = []
 
@@ -119,6 +141,22 @@ secondHM = 0
 thirdHM = 0
 fourthHM = 0
 fifthHM = 0
+
+
+# function for selecting game version and changing Pokemon availability accordingly
+def chooseVersion():
+    global availablePokemonList
+    global gameVersion
+    redExclusives = [Arbok, Vileplume, Primeape, Arcanine, Scyther, Electabuzz]
+    blueExclusives = [Sandslash, Ninetales, Persian, Victreebel, Magmar, Pinsir]
+    versionsList = ["Red", "Blue"]
+    gameVersion = random.choice(versionsList)
+    if gameVersion == "Red":
+        for i in blueExclusives:
+            availablePokemonList.remove(i)
+    elif gameVersion == "Blue":
+        for i in redExclusives:
+            availablePokemonList.remove(i)
 
 
 # function for preventing adding Pokemon of the same type as previously selected Pokemon
@@ -195,11 +233,39 @@ def checkMutuallyExclusive(candidate):
     return 1
 
 
+# function for verifying move coverage against all relevant type combinations
+def checkMoveCoverage(candidate):
+    global party
+    firstCheck = [Bug, Fighting, Ground, Ice]
+    totalMoveCoverage = []
+    for i in party:
+        if len(i.coverage) > 0:
+            for j in i.coverage:
+                totalMoveCoverage.append(j)
+    if len(candidate.coverage) > 0:
+        for i in candidate.coverage:
+            totalMoveCoverage.append(i)
+    if all(item in totalMoveCoverage for item in firstCheck):
+        if Electric in totalMoveCoverage:
+            return 1
+        else:
+            if Rock in totalMoveCoverage:
+                if Grass in totalMoveCoverage:
+                    return 1
+                else:
+                    return 0
+            else:
+                return 0
+    else:
+        return 0
+
+
 # function for selecting starter Pokemon
 def selectStarter():
     global party
     global availablePokemonList
     global hmList
+    starters = [Venusaur, Charizard, Blastoise]
     chosenStarter = random.choice(starters)
     party.append(chosenStarter)
     # test code start
@@ -385,7 +451,10 @@ def select6(pokemonPool):
         if checkSameType(potentialMember) == 1:
             if checkHM(potentialMember, 6) == 1:
                 if checkMutuallyExclusive(potentialMember) == 1:
-                    success = 1
+                    if checkMoveCoverage(potentialMember) == 1:
+                        success = 1
+                    else:
+                        pokemonPool.remove(potentialMember)
                 else:
                     pokemonPool.remove(potentialMember)
             else:
@@ -452,6 +521,8 @@ def displayParty():
             print(party[i].HMs[0].name + ", " + party[i].HMs[1].name + ", " + party[i].HMs[2].name)
 
 
+chooseVersion()
 if selectStarter() == 1:
+    print(gameVersion + " Version")
     sortParty()
     displayParty()
